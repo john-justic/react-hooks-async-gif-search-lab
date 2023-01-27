b6a05313a01e9a6aa6eaab11830c18e6e152dd93
John <johnjustice766@gmail.com>
2023-01-27 09:13:18 +0300

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,18 @@
+import './App.css'
 import React from "react";
 
 import NavBar from "./NavBar";
+import GifListContainer from './GifListContainer'
 
 // the App component should render out the GifListContainer component
 
 function App() {
   return (
-    <div>
+    <div >
+      <h1>Find Happiness With John Jusice</h1>
       <NavBar color="black" title="Giphy Search" />
+      <p>Follow me on insta <a href="https://instagram.com/khalil_.sa?igshid=YmMyMTA2M2Y=" alt="my account">@Khalil_.sa</a></p>
+      <GifListContainer/>
     </div>
   );
 }
